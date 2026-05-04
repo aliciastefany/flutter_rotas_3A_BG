33b0201e19bb501c6c741574c91b8f94e0ffef81
Alícia Stefany <64162281+aliciastefany@users.noreply.github.com>
2026-05-04 18:11:03 +0000

    update drawer

---
+++ flutter_application_rotas/README.md
@@ -0,0 +1,3 @@
+# flutter_application_rotas
+
+A new Flutter project.
---
+++ flutter_application_rotas/android/app/src/main/kotlin/com/example/flutter_application_rotas/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_application_rotas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
